86fa1d99e2362201bdad0bd11f8b178519c16b61
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-10-04 15:46:42 -0300

    D1

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={ Login } />
+        <Route exact path="/" component={ Login } />
         <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
         <Route path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="" component={ NotFound } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     );
   }
